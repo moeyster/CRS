11679cfa2b4c62e506265cfca8dac650cb5a6434
Tony Moey <moeyster@gmail.com>
2015-02-19 06:24:10 +0800

    Added model for clients and the ability to add new clients and display a list of all clients added with currency and date/time formatting

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,10 +1,29 @@
 class ClientsController < ApplicationController
   def index
+  	@clients = Client.all.order(updated_at: :desc)
   end
 
   def new
+  	@client = Client.new
+  end
+
+  def create
+  	@client = Client.new(client_params)
+
+  	if @client.save
+  		redirect_to clients_path, notice: "Successfully added new client! Congratulations!"
+  	else
+  		flash[:alert] = "Sorry, client not added, please try again."
+  		render :new
+  	end
   end
 
   def edit
+  end
+
+  private
+
+  def client_params
+  	params.require(:client).permit(:name, :mt4_id, :capital_sold)	
   end
 end
