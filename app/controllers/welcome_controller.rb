7cc0c047cb216bf318bb397ab96269698bea05e9
Tony Moey <moeyster@gmail.com>
2015-02-22 23:19:05 +0800

    agents can now view their own client list, CRS admin is not displayed on index page)

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,16 @@
 class WelcomeController < ApplicationController
   def index
   	if agent_signed_in?
+  		@agents = Agent.where("id != '8'")
 	  	@clients = Client.all
 	  	@totalClients = Client.count
 	  	@totalCapitalSold = Client.sum(:capital_sold)
 	else
 		redirect_to new_agent_session_path
 	end
+  end
+
+  def show
+  	@agent = Agent.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :agents
 
   resources :clients
+  resources :welcome
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
