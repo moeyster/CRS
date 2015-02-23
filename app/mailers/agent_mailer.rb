81ebdad5e45865a57132db81f68812a916fe7ea4
Tony Moey <moeyster@gmail.com>
2015-02-23 16:36:29 +0800

    fixed agent_mailer.rb bug

--- app/mailers/agent_mailer.rb
+++ app/mailers/agent_mailer.rb
@@ -1,5 +1,5 @@
 class AgentMailer < ApplicationMailer
 	def registration_confirmation(agent)
-		mail(:to => agent.email), :subject => "Registered", :from => "admin@crs.com")
+		mail(:to => agent.email, :subject => "Registered", :from => "admin@crs.com")
 	end
 end
