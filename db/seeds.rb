6e5387581589d3f3d7714c7e7474da39013b9ded
Tony Moey <moeyster@gmail.com>
2015-02-23 16:21:19 +0800

    updated seed file to create admin account with admin field set to true

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-agent = Agent.create(email: "admin@gmail.com", password: "crsAdmin##", name: "CRS Admin")
+agent = Agent.create(email: "admin@gmail.com", password: "crsAdmin##", name: "CRS Admin", admin: true)
