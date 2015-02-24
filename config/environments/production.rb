e8b56e3ce1399065fe88b66c1b58f21f94e1c8bd
Tony Moey <moeyster@gmail.com>
2015-02-24 12:46:15 +0800

    changed default url to crsbysm.herokuapp.com on deployable branch

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # For mailer functionality
-  config.action_mailer.default_url_options = { host:'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host:'crs.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   :user_name => '2558759e72b6537ed',
